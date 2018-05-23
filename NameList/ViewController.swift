cc6601060a31baf32579bb8545e77ac3bfc00685
Shelly Gupta <gshelly1402@gmail.com>
2018-05-23 11:00:46 +0530

    Added Search functionality and section Index for each name

--- NameList/ViewController.swift
+++ NameList/ViewController.swift
@@ -11,8 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     var names: [String] = []
+    var filteredName =  [String: [String]]()
     var alphabetizedName = [String: [String]]()
     var sortedKeys: [String]?
+    var index = 0
+    //By intializing the search view controller with nil, we tell the seach controller the we want to use the same view for displaying the result
+    let searchViewController = UISearchController(searchResultsController: nil)
+    var searchCheck = false
+    
+    @IBOutlet weak var namesTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +34,22 @@ class ViewController: UIViewController {
         } catch let error {
             print("Failed to load : \(error.localizedDescription)")
         }
+
         alphabetizedName = alphabatizedArray(namesList: names)
+        filteredName = alphabetizedName
+        
         //sort the keys
-        sortedKeys = alphabetizedName.keys.sorted(by: <)
+        sortedKeys = filteredName.keys.sorted(by: <)
+        
+        
+        //search controller
+        searchViewController.searchResultsUpdater = self
+        searchViewController.hidesNavigationBarDuringPresentation = false
+        searchViewController.dimsBackgroundDuringPresentation = false
+        namesTableView.tableHeaderView = searchViewController.searchBar
+        
     }
 
-    
     func alphabatizedArray(namesList:[String]) -> [String:[String]] {
         var result = [String :[String]]()
         
@@ -54,6 +71,7 @@ class ViewController: UIViewController {
         }
         return result
     }
+    
 }
 
 extension ViewController : UITableViewDelegate, UITableViewDataSource {
@@ -66,7 +84,7 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
         //fetch the names in a section
         let key = sortedKeys![section]
         
-        if let name = alphabetizedName[key] {
+        if let name = filteredName[key] {
             return name.count
         }
         return 0
@@ -77,8 +95,8 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
         
         //assign the name in cell
         let key = sortedKeys![indexPath.section]
-        
-        if let name = alphabetizedName[key] {
+    
+        if let name = filteredName[key] {
             cell.textLabel?.text = name[indexPath.row]
         }
         return cell
@@ -87,10 +105,14 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sortedKeys![section]
     }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sortedKeys
+    }
 }
 extension String {
     func getSubstringFromStart(num :Int) -> String? {
-        if self.count > num {
+        if self.count >= num {
             let index = self.index(self.startIndex, offsetBy: num)
             //capturing first letter of the string with the help of index
             let subString = self[..<index].uppercased()
@@ -98,5 +120,26 @@ extension String {
         }
         return nil
     }
+}
+// TO Allow view controller to respond to the search bar, it will have to implement UISearchResultsUpdating
+//This protocol defines the method to update the search result based on the information the user enters into the search bar
+extension ViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        if let searchText = searchViewController.searchBar.text, searchText.count > 0{
+            let firstCharacter = searchText.uppercased().getSubstringFromStart(num: 1)!
+            let valueArray = alphabetizedName[firstCharacter]?.filter({ (value) -> Bool in
+                return value.lowercased().contains(searchText.lowercased())
+            })
+            filteredName = [firstCharacter : valueArray] as! [String : [String]]
+     }
+    else {
+            filteredName = alphabetizedName
+    }
+        sortedKeys = filteredName.keys.sorted(by: <)
+        namesTableView.reloadData()
+}
+    
+    
 }
 
